09580eaa59e074a1e0bf58341e8d7cc4d0dd166c
anastasionico <nico@anastasionico.uk>
2017-09-21 00:21:39 +0100

    smacss scaffolding created

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,54 +3,6 @@
 <head>
 	<title>@yield('title')</title>
 	<link rel="stylesheet" type="text/css" href="css/app.css">
-	<style type="text/css">
-		body{
-			margin: 0;
-			padding: 0;
-			font-size: 17px;
-		}
-		
-		header{
-			position: absolute;
-			top: 0;
-			bottom: 0;
-			left: 0;
-			padding-top: 1rem;
-			width: 5%;
-			background-color: #35a;
-			opacity: 0.5;
-
-		}
-		.hamburger{
-			display: block;
-			margin: 4px auto;
-			background-color: #fff;
-			width: 66%;
-			height: 2px;
-		}
-		
-		.mainSection{
-			padding: 0 10%;
-			width: 100%;
-			background-color: #3aa;
-		}
-		
-		aside{
-			position: absolute;
-			top: 0;
-			right: 0;
-			bottom: 0;
-			width: 5%;
-			background-color: #35a;
-			opacity: 0.5;
-		}
-
-		footer{
-			padding: 0 10%;
-			width: 100%;
-			background-color: #35a;
-		}
-	</style>
 </head>
 <body>
 	<header>
