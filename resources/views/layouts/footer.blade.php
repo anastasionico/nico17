413968742cf2bc6762fe97d8760489d6a529eaa0
anastasionico <nico@anastasionico.uk>
2017-09-29 20:37:25 +0100

    mobile footer styled

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,77 @@
 <footer>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
-	this is the footer<br>
+	<div class="footer--section">
+		
+		<div class="twoThird">
+			<div class="twoThird--small">
+				<div class="twoThird--small-content">
+					<h3>Anastasio Nico</h3>
+					<small>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					<<small>
+				</div>
+			</div>
+			<div class="twoThird--big">
+				<div class="twoThird--big-content">
+					<form class="footerForm">
+						<input type="text" name="name">
+						<input type="text" name="email">
+						<textarea rows="1" cols="33"></textarea>
+						<input class="btn" type="submit" name="SEND">
+					</form>
+				</div>
+			</div>
+			
+
+			<div class="sectionCenter">
+				<div class="sectionCenter--content">
+					<span  class="social-logo mt-1 smallCircle flexCenter" id="social-1">
+						html
+					</span>
+				</div>
+				<div class="sectionCenter--content">
+					<span  class="social-logo mt-1 smallCircle flexCenter" id="social-2">
+						Css
+					</span>
+				</div>
+				<div class="sectionCenter--content">
+					<span  class="social-logo mt-1 smallCircle flexCenter" id="social-3">
+						Sass
+					</span>
+				</div>
+			</div>
+
+			<div class="footer--section-blog">
+				<ul>
+					<li><b>Coding</b></li>
+					<li>link one</li>
+					<li>link two</li>
+					<li>link three</li>
+				</ul>
+				<ul>
+					<li><b>Graphics</b></li>
+					<li>link one</li>
+					<li>link two</li>
+					<li>link three</li>
+				</ul>				
+				<ul>
+					<li><b>Utility</b></li>
+					<li>link one</li>
+					<li>link two</li>
+					<li>link three</li>
+				</ul>
+			</div>
+			
+			<div class="footer--section-copyright">
+				<small>Copyright 2017 Anastasio Domenico</small>
+			</div>
+
+
+
+		</div>	
+	</div>
+	
+	
+
+	
 </footer>
