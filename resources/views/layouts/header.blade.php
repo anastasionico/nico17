a235035bebb620f993d74db52baf513161bd0bc0
anastasionico <nico@anastasionico.uk>
2017-10-04 23:28:21 +0100

    created layout and style header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,22 +1,29 @@
 <header class="visible">
-
-	<div class="hamburger">
-		<span class="hamburgerLine-1">&nbsp;</span>
-	    <span class="hamburgerLine-2">&nbsp;</span>
-	    <span class="hamburgerLine-3">&nbsp;</span>
+	<div class='links'>
+		<nav>
+			<a href="#">My Projects</a>
+			<a href="#">Articles</a>
+			<a href="#">Contact</a>
+			<a href="#">F</a>
+			<a href="#">Lin</a>
+			<a href="#">Up</a>
+			<a href="#">Git</a>
+		</nav>
 	</div>
+	<div class="hamburgerDiv">
+		<div class="hamburger">
+			<span class="hamburgerLine-1">&nbsp;</span>
+		    <span class="hamburgerLine-2">&nbsp;</span>
+		    <span class="hamburgerLine-3">&nbsp;</span>
+		</div>
+	</div>
+
 	
-	<nav>
-		<a href="#">link 1</a>
-		<a href="#">link 1</a>
-		<a href="#">link 1</a>
-		<a href="#">link 1</a>
-	</nav>
-	look header.blade.php
+
 </header>
 
 {{-- create a 2 column layout for the header put the nav in the left colomn the hamburger in the right column.
 both at the center of the screen
-the header visible has to be 50% of the page (maybe more)
-add the jquery and a transition effect with scss
+the header visible has to be 25% of the page 
+add the javascript and a transition effect with scss
  --}}
