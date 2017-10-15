b9a8f90c56c09838d101408221f0f9edef3156ac
anastasionico <nico@anastasionico.uk>
2017-10-15 12:57:27 +0100

    added the gist code block to the detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -81,12 +81,26 @@
 		<div class="oneThird" >
 			<div class="oneThird--big">
 				<div class="oneThird--big--content">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+			<div class="oneThird--small">&nbsp;</div>
+		</div>		
+
+		
+		
+
+		<div class="oneThird" >
+			<div class="oneThird--big">
+				<div class="oneThird--big--content">
+					<script src="https://gist.github.com/xfbs/1188092.js"></script>
 				</div>
 			</div>
 			<div class="oneThird--small">&nbsp;</div>
@@ -191,4 +205,5 @@
 	</section>
 	
 @endsection
+
 
