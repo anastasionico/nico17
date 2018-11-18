65245ddded97da99052625240ba3d4a13810761a
anastasionico <nico@anastasionico.uk>
2018-11-18 11:36:52 +0000

    add twitter button to detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -158,7 +158,9 @@
 					<a href="https://www.facebook.com/sharer/sharer.php?u={{$actual_link}}" title="Share on Facebook" rel="nofollow" target="_blank" class="bigCircle flexCenter bg-white shareButton-div-mobile-circle">
 						<i class="fa fa-2x fa-facebook" aria-hidden="true"></i>
 					</a>
-					
+					<a href="http://twitter.com/share?text={{ ucfirst($content->excerpt) }}&url={{$actual_link}}&hashtags={{str_replace(' ', '', ucwords($content->keywords))}}"  class="bigCircle flexCenter bg-white shareButton-div-mobile-circle">
+						<i class="fa fa-2x fa-twitter" aria-hidden="true"></i>
+					</a>
 					<a href="https://www.linkedin.com/shareArticle?mini=true&url={{$actual_link}}&title={{ ucfirst($content->name) }}&summary={{ ucfirst($content->excerpt) }}&source=anastasionico.uk" title="Share on LinkedIn" rel="nofollow" target="_blank" class="bigCircle flexCenter bg-white shareButton-div-mobile-circle">
 						<i class="fa fa-2x fa-linkedin" aria-hidden="true"></i>
 					</a>	
